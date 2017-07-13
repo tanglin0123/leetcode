b4c391f4e371fe97eac852045195be55f18d8085
sony <sony@sony-VAIO>
2017-07-13 23:36:24 +0800

    quicksort

--- leetcode/src/interview/QuickSort.java
+++ leetcode/src/interview/QuickSort.java
@@ -5,6 +5,50 @@ public class QuickSort {
 		this.sort(nums, 0, nums.length -1);
 	}
 	
+	public void sort2(int[] nums){
+		this.sort2(nums, 0, nums.length -1);
+	}
+	
+	public void sort2(int[] nums, int start, int end){
+		if(start >= end){
+			return;
+		}
+		
+		int i = start; 
+		int j = end; 
+		
+		while(i < j){
+			int v = nums[i];
+			while(j > i && nums[j] > v){
+				--j;
+			}
+			
+			if(j > i) {
+				nums[i] = nums[j];
+				nums[j] = v; 
+				++i;
+			}
+			
+			
+			while(i < j && nums[i] <= v){
+				++i;
+			}
+			
+			if(i < j) {
+				nums[j] = nums[i];
+				nums[i] = v;
+				--j;
+				++i;
+			}
+			
+		}
+		
+		nums[i] = key;
+		sort2(nums, start, i -1);
+		sort2(nums, i + 1, end);
+		
+	}
+	
 	public void sort(int[] nums, int start, int end){
 		if(start >= end){
 			return;
@@ -15,7 +59,7 @@ public class QuickSort {
 		int key = nums[i];
 		
 		while(i < j){
-			while(j > i && nums[j] >= key){
+			while(j > i && nums[j] > key){
 				--j;
 			}
 			
@@ -35,8 +79,14 @@ public class QuickSort {
 	}
 	
 	public static void main(String[] args) {
-		
+		int[] a = {1,5,2,3,2,6,4,7,10,5};
 
+		QuickSort q = new QuickSort();
+		q.sort(a);
+		
+		for(int i : a) {
+			System.out.print(i + ", ");
+		}
 	}
 
 }
