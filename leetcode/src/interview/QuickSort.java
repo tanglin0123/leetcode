507a74d86f6067ddca01c935b44038c08cd152d8
Forest Soup <tanglin0123@126.com>
2017-07-14 10:34:58 +0800

    Update QuickSort.java

--- leetcode/src/interview/QuickSort.java
+++ leetcode/src/interview/QuickSort.java
@@ -1,5 +1,7 @@
 package interview;
 
+import java.util.*;
+
 public class QuickSort {
 	public void sort(int[] nums){
 		this.sort(nums, 0, nums.length -1);
@@ -7,6 +9,52 @@ public class QuickSort {
 	
 	public void sort2(int[] nums){
 		this.sort2(nums, 0, nums.length -1);
+	}
+	
+	public void sortWithoutRecursive(int[] nums){
+		if(nums == null || nums.length <= 1){
+			return;
+		}
+		
+		Stack<int[]> stk = new Stack<int[]>();
+		
+		stk.push(new int[]{0, nums.length-1});
+		
+		while(!stk.isEmpty()){
+			int[] idx = stk.pop();
+			
+			int i = idx[0];
+			int j = idx[1];
+			
+			int v = nums[i];
+			
+			while(i < j){
+				while(j > i && nums[j] > v){
+					--j;
+				}
+				
+				if(j > i) {
+					nums[i] = nums[j];
+				}
+				
+				while(i < j && nums[i] <= v){
+					++i;
+				}
+				
+				if(i < j){
+					nums[j] = nums[i];
+				}
+			}
+			
+			nums[i] = v;
+			
+			if(idx[0] < i-1){
+				stk.push(new int[]{idx[0], i-1});
+			}
+			if(i+1 < idx[1]){
+				stk.push(new int[]{i+1, idx[1]});
+			}
+		}
 	}
 	
 	public void sort2(int[] nums, int start, int end){
@@ -79,8 +127,18 @@ public class QuickSort {
 		int[] a = {1,5,2,3,2,6,4,7,10,5};
 
 		QuickSort q = new QuickSort();
-		q.sort(a);
 		
+		q.sort(a);
+		for(int i : a) {
+			System.out.print(i + ", ");
+		}
+		
+		q.sort2(a);
+		for(int i : a) {
+			System.out.print(i + ", ");
+		}
+		
+		q.sortWithoutRecursive(a);
 		for(int i : a) {
 			System.out.print(i + ", ");
 		}
