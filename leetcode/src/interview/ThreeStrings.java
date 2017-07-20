afb6b22634a4d1caaebdd8967331b08da8d5a5c6
tanglin <tanglin0123@126.com>
2017-07-20 23:07:31 +0800

    update 3 strs

--- leetcode/src/interview/ThreeStrings.java
+++ leetcode/src/interview/ThreeStrings.java
@@ -3,7 +3,26 @@ package interview;
 // amazon interview
 public class ThreeStrings {
 	public boolean validate(String a, String b, String c) {
+		if(c == null || c.isEmpty()) {
+			return false;
+		}
 		
+		if(a == null || a.isEmpty()) {
+			return c.equals(b);
+		}
+		
+		if(b == null || b.isEmpty()) {
+			return c.equals(a);
+		}
+		
+		int[][] dp = new int[c.length() + 1][2];
+		dp[0][0] = -1;
+		dp[0][1] = -1;
+		
+		for(int i = 0; i < c.length(); ++i) {
+			char ch = c.charAt(i);
+			int apos = a.indexOf(ch, dp[i-1])
+		}
 	}
 	
 	public static void main(String[] args) {
