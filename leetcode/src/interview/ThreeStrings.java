757d66ccb65cc70f9cdb1d3bbbfa19c754db86b8
tanglin <tanglin0123@126.com>
2017-07-20 22:50:54 +0800

    amazone interview 3 strings

---
+++ leetcode/src/interview/ThreeStrings.java
@@ -0,0 +1,13 @@
+package interview;
+
+// amazon interview
+public class ThreeStrings {
+	public boolean validate(String a, String b, String c) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
