338a24118514acc593589ce840dcf7b3527805ac
tanglin <tanglin0123@126.com>
2017-08-10 13:04:58 +0800

    ExceptionNumber

---
+++ leetcode/src/atry/Test.java
@@ -0,0 +1,19 @@
+package atry;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(bitCount(1));
+		System.out.println(bitCount(2));
+		System.out.println(bitCount(3));
+		System.out.println(bitCount(4));
+		System.out.println(bitCount(5));
+		System.out.println(bitCount(100));
+	}
+	
+	static int bitCount(int n) {
+		return (int)Math.ceil((Math.log(n)/Math.log(2)));
+	}
+	
+
+}
---
+++ leetcode/src/interview/ExceptionNumber.java
@@ -0,0 +1,45 @@
+package interview;
+
+// learn from leetcode p137 
+// https://discuss.leetcode.com/topic/22821/an-general-way-to-handle-all-this-sort-of-questions
+public class ExceptionNumber {
+	// given a array of int, every one is occurs m times, except one(not told occur time), 
+	// find out the exception one 
+//	public int findException(int[] nums, int m) {
+//		int len = (int)Math.ceil((Math.log(m)/Math.log(2)));
+//		int exNum = nums.length % m;
+//		
+//		
+//	}
+	
+	// given a array of int, every one is occurs 3 times, except one which occur times is not told, 
+	// find out the exception one 
+	public int findException_m3(int[] nums) {
+		int b1 = 0, b2 = 0;
+		int exNum = nums.length % 3;
+		
+		for(int n : nums) {
+			int newb1 = (b1 & ~b2 & ~n) | (~b1 & b2 & n);
+			int newb2 = (~b1 & b2 & ~n) | (~b1 & ~b2 & n);
+			
+			b1 = newb1;
+			b2 = newb2;
+		}
+		
+		if(exNum == 1) {
+			return b2;
+		} else { // exNum ==2
+			return b1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {3,1,1,2,3,1,3};
+//		int[] a = {3,2,1,1,2,3,1,2};
+		
+		ExceptionNumber e = new ExceptionNumber();
+		System.out.println(e.findException_m3(a));
+		
+	}
+
+}
