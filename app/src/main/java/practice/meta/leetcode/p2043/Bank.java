44e909a63dcb334987b18f2574635acf87ede46d
Lin Tang <tanglin0123@126.com>
2025-12-07 15:32:13 -0800

    Meta LC 2043

---
+++ app/src/main/java/practice/meta/leetcode/p2043/Bank.java
@@ -0,0 +1,55 @@
+package practice.meta.leetcode.p2043;
+
+import java.util.*;
+
+class Bank {
+
+    private long[] balance;
+
+    public Bank(long[] balance) {
+        this.balance = balance;    
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+        int src = account1 - 1;
+        int tgt = account2 - 1;
+
+        if (src < 0 || src >= balance.length || tgt < 0 || tgt >= balance.length ) {
+            return false;
+        }
+
+        if (balance[src] >= money) {
+            balance[src] -= money;
+            balance[tgt] += money;
+            return true;
+        }
+
+        return false;
+    }
+    
+    public boolean deposit(int account, long money) {
+        int tgt = account - 1;
+
+        if (tgt < 0 || tgt >= balance.length ) {
+            return false;
+        }
+
+        balance[tgt] += money;
+        return true;
+    }
+    
+    public boolean withdraw(int account, long money) {
+        int tgt = account - 1;
+
+        if (tgt < 0 || tgt >= balance.length ) {
+            return false;
+        }
+
+        if (balance[tgt] < money) {
+            return false;
+        }
+
+        balance[tgt] -= money;
+        return true;
+    }
+}
